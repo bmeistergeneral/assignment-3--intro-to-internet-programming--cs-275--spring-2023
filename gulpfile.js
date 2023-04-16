8e329231cb9e75f9f017271a0ad077856f06662b
bmeistergeneral <109158688+bmeistergeneral@users.noreply.github.com>
2023-04-16 15:24:21 -0400

    Added JS Linter and fixed linting errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, series, watch } = require(`gulp`),
     CSSLinter = require(`gulp-stylelint`);
+    jsLinter = require(`gulp-eslint`);
 
 let lintCSS = () => {
     return src(`./styles/*.css`)
@@ -11,4 +12,12 @@ let lintCSS = () => {
         }));
 };
 
+let lintJS = () => {
+    return src(`./scripts/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
 exports.lintCSS = lintCSS;
+exports.lintJS = lintJS;
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
-let modal = document.querySelector('.modal-panel');
-let modalContent = document.querySelector('.modal-content');
+let modal = document.querySelector(`.modal-panel`);
+let modalContent = document.querySelector(`.modal-content`);
 
-let textContent = document.createElement('p');
+let textContent = document.createElement(`p`);
 modalContent.appendChild(textContent);
-textContent.innerHTML = "Hi, my name is Brecon";
+textContent.innerHTML = `Hi, my name is Brecon`;
 
 
 
 let showMenuLink = document.getElementById(`showMenuLink`);
 let showModalLink = document.getElementById(`showModalLink`);
 
-showMenuLink.addEventListener('click', () => {
-    alert("Testing Menu");
+showMenuLink.addEventListener(`click`, () => {
+    alert(`Testing Menu`);
 });
 
-showModalLink.addEventListener('click', () => {
+showModalLink.addEventListener(`click`, () => {
     modal.classList.remove(`hide-modal-panel`);
 });
 
-modal.addEventListener('click', (e) => {
+modal.addEventListener(`click`, (e) => {
     // If an element that isn't the parent is clicked on, don't do anything. If only 
     // the parent is clicked on, then it hides.
     if (e.target !== e.currentTarget) return;
@@ -27,10 +27,10 @@ modal.addEventListener('click', (e) => {
 });
 
 document.onkeydown = function(e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        modal.classList.add("hide-modal-panel");
+    if (e.key === `Escape` || e.key === `Esc`) {
+        modal.classList.add(`hide-modal-panel`);
     }
-}
+};
 
 
 
