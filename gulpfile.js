f0795f993731e655c73becadd00b82e4d09c1465
bmeistergeneral <109158688+bmeistergeneral@users.noreply.github.com>
2023-04-16 14:56:18 -0400

    Gulp command for CSS Linting and fixed linting errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-// testing Git
+const { src, dest, series, watch } = require(`gulp`),
+    CSSLinter = require(`gulp-stylelint`);
+
+let lintCSS = () => {
+    return src(`./styles/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
+exports.lintCSS = lintCSS;
