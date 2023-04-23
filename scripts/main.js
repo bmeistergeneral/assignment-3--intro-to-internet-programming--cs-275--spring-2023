4349b5c520079a667ee56d6aa922fbe49ed037d6
bmeistergeneral <109158688+bmeistergeneral@users.noreply.github.com>
2023-04-23 15:01:12 -0400

    Created browser resize event listener that detects if threshold is crossed

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,9 @@ textContent.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit
 let showMenuLink = document.getElementById(`showMenuLink`);
 let showModalLink = document.getElementById(`showModalLink`);
 
+let oldViewportWidth = window.innerWidth;
+let viewportThreshold = 736;
+
 showMenuLink.addEventListener(`click`, () => {
     // Doing this here because viewport width will change over time.
     let viewportWidth = window.innerWidth;
@@ -48,5 +51,14 @@ document.onkeydown = function(e) {
     }
 };
 
+window.addEventListener(`resize`, (e) => {
+    let newViewportWidth = window.innerWidth;
+
+    if ((oldViewportWidth < viewportThreshold && newViewportWidth > viewportThreshold) ||
+        (oldViewportWidth > viewportThreshold && newViewportWidth < viewportThreshold)) {
+        
+        document.location.reload(true);
+    }
+});
 
 
